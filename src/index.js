915b90ad47300caee23325698a64c516fe558c90
anshuman7negi <anshumannegi777@gmail.com>
2023-07-12 18:33:29 +0530

    Created checkbox function to display completed items

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-import AddList from "./tudo.js";
+import AddList from './tudo.js';
+
 const myList = new AddList();
 myList.displayList();
 
 const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', () => {
   const task = document.getElementById('task').value.trim();
-  const completed=false;
-  const index=myList.todoDetails.length + 1;
-  if(task){
-    myList.addRow(task,completed,index);
+  const completed = false;
+  const index = myList.todoDetails.length + 1;
+  if (task) {
+    myList.addRow(task, completed, index);
     myList.displayList();
     document.getElementById('task').value = '';
+    todoCheckbox();
   }
 });
 
-const editBtn = document.querySelectorAll('.editBtn');
-editBtn.forEach((editElement) => {
-    editElement.addEventListener('click', (event) => {
-        const index = event.target.getAttribute('data-index');
-        const editInput = document.querySelector(`input.editBtn[data-index="${index}"]`);
-        editInput.readOnly = false;
-        editInput.addEventListener('blur', (event) => {
-            const newTitle = event.target.value;
-            myList.updateRowTitle(index, newTitle);
-            event.target.readOnly = true;
-        });
+function todoCheckbox() {
+  const checkboxes = document.querySelectorAll('.edit-text');
+  checkboxes.forEach((checkbox) => {
+    const index = checkbox.parentNode.querySelector('.editBtn').getAttribute('data-index');
+    const editInput = checkbox.parentNode.querySelector('.editBtn');
+    const { completed } = myList.todoDetails[index];
+
+    checkbox.checked = completed;
+    editInput.classList.toggle('completed', completed);
+
+    checkbox.addEventListener('change', (event) => {
+      const isChecked = event.target.checked;
+      editInput.classList.toggle('completed', isChecked);
+      myList.todoDetails[index].completed = isChecked;
+      localStorage.setItem('todoData', JSON.stringify(myList.todoDetails));
     });
-});
+  });
+}
+
+window.addEventListener('DOMContentLoaded', todoCheckbox);
