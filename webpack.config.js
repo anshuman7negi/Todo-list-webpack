f383f8f155f2c3a1405144b6c4142150dc2b49b4
anshuman7negi <anshumannegi777@gmail.com>
2023-07-13 14:04:54 +0530

    css bug fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 
 module.exports = {
+  mode: 'production',
   entry: {
     index: './src/index.js',
   },
-  mode: 'development',
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.resolve(__dirname, 'dist'), // Set the static directory to 'src'
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+      css: ['style.css'],
     }),
-    new NodePolyfillPlugin(),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/Todo-list-webpack/',
   },
   module: {
     rules: [
@@ -26,19 +31,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
   resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      child_process: false,
-      fs: false,
-      os: false,
-      path: false,
-    },
+    extensions: ['.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
-
 };
